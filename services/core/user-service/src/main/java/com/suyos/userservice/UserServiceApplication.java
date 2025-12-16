f96f696ecb5db90a745d24abdab700e7d1cf225f
JSalazarAlt <ed.joel.salazar@gmail.com>
2025-12-15 22:04:08 -0500

    Add missing annotation for auditory fields

--- services/core/user-service/src/main/java/com/suyos/userservice/UserServiceApplication.java
+++ services/core/user-service/src/main/java/com/suyos/userservice/UserServiceApplication.java
@@ -2,6 +2,7 @@ package com.suyos.userservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 /**
  * Main application class for the User microservice.
@@ -11,6 +12,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
  * complete user management functionality.</p>
  */
 @SpringBootApplication
+@EnableJpaAuditing
 public class UserServiceApplication {
 
 	/**
